212a101254b330adcb8bbd6e1ce799a371057503
Douglas <nkubitudouglas@gmail.com>
2023-05-20 02:19:16 +0300

    feat: member group doctype

---
+++ moi/moi/doctype/member_group/member_group.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Douglas Nkubitu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class MemberGroup(NestedSet):
+	pass
