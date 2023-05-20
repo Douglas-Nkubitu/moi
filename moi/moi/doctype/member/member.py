bdfd51655ee19334d8e9834b5695b044d1c3f5c4
Douglas <nkubitudouglas@gmail.com>
2023-05-20 10:19:55 +0300

    fix: removed validate user function

--- moi/moi/doctype/member/member.py
+++ moi/moi/doctype/member/member.py
@@ -10,7 +10,6 @@ class Member(Document):
 		self.set_full_name()
 		self.validate_email()
 		self.validate_mobile_no()
-		self.validate_user()
 		
 	def set_full_name(self):
 		self.full_name = " ".join(filter(None, [self.first_name, self.last_name]))
@@ -20,15 +19,3 @@ class Member(Document):
 
 	def validate_email(self):
 		validate_email_address(self.email.strip(), True)
-
-	def validate_user(self):
-		exists = frappe.db.exists(
-			"Member",
-			{
-				#check if there's a member using the same mobile number 
-				"mobile_no": self.mobile_no,
-				"docstatus": 1,
-			},
-		)
-		if exists:
-			frappe.throw(f"{self.full_name} is already Registed")
