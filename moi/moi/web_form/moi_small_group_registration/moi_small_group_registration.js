f7414f4ac1911ebfc2e2a84497663cee5ec1fa5c
Douglas <nkubitudouglas@gmail.com>
2023-06-09 20:40:30 +0300

    fix: replace age group select with link

--- moi/moi/web_form/moi_small_group_registration/moi_small_group_registration.js
+++ moi/moi/web_form/moi_small_group_registration/moi_small_group_registration.js
@@ -1,3 +1,14 @@
 frappe.ready(function() {
-	// bind events here
+	frappe.web_form.on('age_group', (field, value) => {
+		frappe.call({
+			method: 'moi.moi.doctype.member.member.allocate_small_group',
+			args: {
+				age_group: value
+			},
+			callback: function(response) {
+				frappe.web_form.set_value('moi_small_group', response.message.team_name);
+				frappe.web_form.set_df_property('moi_small_group', 'read_only', 1);
+			}
+		});
+	});
 })
