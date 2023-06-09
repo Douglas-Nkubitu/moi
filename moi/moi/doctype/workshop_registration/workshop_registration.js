efb117e657d43317b0b75192b92731797c83aa1e
Douglas <nkubitudouglas@gmail.com>
2023-06-09 21:16:45 +0300

    chore: workspace update

--- moi/moi/doctype/workshop_registration/workshop_registration.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Douglas Nkubitu and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Workshop Registration', {
-	// refresh: function(frm) {
-
-	// }
-});
